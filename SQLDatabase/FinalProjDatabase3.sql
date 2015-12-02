4f1073530d455603f03b2f7c0dba8770433ff991
Kevin Tie <Ksiantie@wl-10-190-215-60.wireless.duke.edu>
2015-12-02 01:19:52 -0500

    updated database

--- SQLDatabase/FinalProjDatabase3.sql
+++ SQLDatabase/FinalProjDatabase3.sql
@@ -70,14 +70,14 @@ FOR EACH ROW
 EXECUTE PROCEDURE RelationshipTF();
 
 
-INSERT INTO UserInf VALUES(0, 'Anna', 'female','anna@duke.edu', '1994-03-05', '2153783609', 'Philadelphia', '2015-11-11','relationship', 'male');
-INSERT INTO USERINF VALUES(1, 'Will','male', 'will@duke.edu', '1996-06-08', '1234567890', 'New York', '2015-12-15' , 'fwb', 'female');
-INSERT INTO USERINF VALUES(2, 'Dan', 'male', 'dan@duke.edu', '1987-03-01', '9087654321', 'Boston', '2015-10-11' , 'relationship', 'female');
-INSERT INTO USERINF VALUES(3, 'Kelly', 'female', 'kelly@duke.edu', '1987-10-10', '9087987634', 'Boston', '2015-09-11' , 'fwb', 'male');
-INSERT INTO USERINF VALUES(4, 'Emily', 'female', 'emily@duke.edu', '1987-05-14', '8765567888', 'Toledo', '2015-12-11' , 'relationship', 'both');
-INSERT INTO USERINF VALUES(5, 'Ian',  'male','ian@duke.edu', '1990-03-01', '9765123456', 'New York', '2015-10-09' , 'fwb', 'both');
-INSERT INTO USERINF VALUES(6, 'Sarah', 'female', 'sarah@duke.edu', '1967-03-01', '9090909090', 'Tampa', '2015-12-11' , 'relationship', 'both');
-INSERT INTO USERINF VALUES(7, 'Sophie', 'female', 'sophie@duke.edu', '1997-04-01', '3245234512', 'New York', '2015-4-11' , 'relationship', 'male');
+INSERT INTO UserInf VALUES(0, 'Anna', 'asodfim3', 'female','anna@duke.edu', '1994-03-05', '2153783609', 'Philadelphia', '2015-11-11','relationship', 'male');
+INSERT INTO USERINF VALUES(1, 'Will', 'd98aji3maf', 'male', 'will@duke.edu', '1996-06-08', '1234567890', 'New York', '2015-12-15' , 'fwb', 'female');
+INSERT INTO USERINF VALUES(2, 'Dan', 'a892m3mawn3', 'male', 'dan@duke.edu', '1987-03-01', '9087654321', 'Boston', '2015-10-11' , 'relationship', 'female');
+INSERT INTO USERINF VALUES(3, 'Kelly', '8ja@*@#RFIU82', 'female', 'kelly@duke.edu', '1987-10-10', '9087987634', 'Boston', '2015-09-11' , 'fwb', 'male');
+INSERT INTO USERINF VALUES(4, 'Emily', '8M*m8fnasdn8', 'female', 'emily@duke.edu', '1987-05-14', '8765567888', 'Toledo', '2015-12-11' , 'relationship', 'both');
+INSERT INTO USERINF VALUES(5, 'Ian', 'iaofa3 1  a 23', 'male','ian@duke.edu', '1990-03-01', '9765123456', 'New York', '2015-10-09' , 'fwb', 'both');
+INSERT INTO USERINF VALUES(6, 'Sarah', 'LOLOLOLOLOL', 'female', 'sarah@duke.edu', '1967-03-01', '9090909090', 'Tampa', '2015-12-11' , 'relationship', 'both');
+INSERT INTO USERINF VALUES(7, 'Sophie', '(*&@!*#&!(@&#(*', 'female', 'sophie@duke.edu', '1997-04-01', '3245234512', 'New York', '2015-4-11' , 'relationship', 'male');
 
 INSERT INTO UserInterests VALUES(0,  'books');
 INSERT INTO UserInterests VALUES(0, 'movies');
