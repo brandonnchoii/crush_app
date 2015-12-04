0d4cd3217d6633ba2096c66f8027a5a8fc0d7de8
Kevin Tie <Ksiantie@wl-10-190-215-60.wireless.duke.edu>
2015-12-04 02:20:59 -0500

    suggestions added

---
+++ SQLDatabase/Suggestions.sql
@@ -0,0 +1,60 @@
+--input user as "u1id"
+
+WITH
+u1int (interests) AS
+	(SELECT interest
+	FROM UserInterests
+	WHERE UserInterests.uiid = u1id)
+u1info (uid1, name1, gender1, commitLevel1, interestedIn1) AS
+	(SELECT uid, name, gender, commitLevel, interestedIn
+	FROM UserInf
+	WHERE UserInf.uid = u1id)
+compatibleUsers (uid2, name2, gender2, commitLevel2, interestedIn2) AS
+	(SELECT uid, name, gender, commitLevel, interestedIn
+	FROM UserInf, u1info
+	WHERE
+		((UserInf.gender = u1info.interestedIn1
+		AND (UserInf.interestedIn = u1info.gender1 OR UserInf.interestedIn = "Both"))
+		OR
+		(u1info.gender1 = UserInf.interestedIn
+		AND (u1info.interestedIn1 = UserInf.gender OR u1info.interestedIn1 = "Both"))
+		OR
+		(UserInf.interestedIn = "Both" AND u1info.interestedIn1 = "Both"))
+		AND
+		UserInf.commitLevel = u1info.commitlevel1)
+
+(SELECT uid2 FROM compatibleUsers
+WHERE
+	(SELECT COUNT(*)
+	FROM
+		((SELECT interest
+		FROM UserInterests
+		WHERE compatibleUsers.uid2 = UserInterests.uiid)
+		INTERSECT
+		(SELECT * FROM u1int)) AS T)
+	= 3
+)
+UNION
+(SELECT uid2 FROM compatibleUsers
+WHERE
+	(SELECT COUNT(*)
+	FROM
+		((SELECT interest
+		FROM UserInterests
+		WHERE compatibleUsers.uid2 = UserInterests.uiid)
+		INTERSECT
+		(SELECT * FROM u1int)) AS T)
+	= 2
+)
+UNION
+(SELECT uid2 FROM compatibleUsers
+WHERE
+	(SELECT COUNT(*)
+	FROM
+		((SELECT interest
+		FROM UserInterests
+		WHERE compatibleUsers.uid2 = UserInterests.uiid)
+		INTERSECT
+		(SELECT * FROM u1int)) AS T)
+	= 1
+)
